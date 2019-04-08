82c6d9fb370d0ea098f7d29c66801aa847a70019
John McDonnell <john@mcdonnell.com>
2019-04-08 16:22:59 +0100

    Implement SearchFragment

---
+++ app/src/main/java/com/mcdonnellapps/lastfmtest/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.mcdonnellapps.lastfmtest.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
