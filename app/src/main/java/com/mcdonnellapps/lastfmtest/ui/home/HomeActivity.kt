fa14bba1d34063726f955968067b6f3559d14e1b
John McDonnell <john@mcdonnell.com>
2019-04-08 16:40:00 +0100

    Rudimentary addition of navigation support

--- app/src/main/java/com/mcdonnellapps/lastfmtest/ui/home/HomeActivity.kt
+++ app/src/main/java/com/mcdonnellapps/lastfmtest/ui/home/HomeActivity.kt
@@ -2,11 +2,13 @@ package com.mcdonnellapps.lastfmtest.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mcdonnellapps.lastfmtest.R
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
     }
 
 }
